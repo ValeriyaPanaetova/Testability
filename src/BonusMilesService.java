a48af3db172e0818c28b3c6ce76d730ac7011ddf
Valeriа <Lerusya.Pan@yandex.ru>
2023-06-28 20:37:20 +0700

    All my changes

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int bonusScale = 25;
+        int bonusScale = 20;
         int bonusMile = cost / bonusScale;
         return bonusMile; // возвращаем рассчитанное кол-во бонусных миль
     }
